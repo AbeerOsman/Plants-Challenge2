50d4e91dfa1a4f924b3911638eccff9734c2c320
AbeerOsman <abeerjilani90@gmail.com>
2025-10-17 21:43:45 +0300

    Initial Commit

---
+++ Plants-Challenge2/Plants_Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Plants_Challenge2App.swift
+//  Plants-Challenge2
+//
+//  Created by Abeer Jeilani Osman  on 25/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Plants_Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Plants-Challenge2Tests/Plants_Challenge2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Plants_Challenge2Tests.swift
+//  Plants-Challenge2Tests
+//
+//  Created by Abeer Jeilani Osman  on 25/04/1447 AH.
+//
+
+import Testing
+@testable import Plants_Challenge2
+
+struct Plants_Challenge2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
